ae0fcf76c88d0792f1ae44548cc60af2760c542d
slawo <slawekpettke@gmail.com>
2022-07-18 17:48:45 +0200

    ---

--- src/main/java/ex802Average/AverageOfPositiveNumbers.java
+++ src/main/java/ex802Average/AverageOfPositiveNumbers.java
@@ -1,12 +1,29 @@
 package ex802Average;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> positiveNumbers = new ArrayList<>();
+        int sum = 0;
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == 0) {
+                break;
+            }
+            if (number > 0) {
+                positiveNumbers.add(number);
+            }
+        }
+        for (Integer number : positiveNumbers) {
+            sum += number;
+        }
+        System.out.println((double) (sum / positiveNumbers.size()));
     }
 }
