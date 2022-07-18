01be16afadf9b0b0741d89bbc94c14cf317daf72
slawo <slawekpettke@gmail.com>
2022-07-18 20:22:37 +0200

    ---

---
+++ src/main/java/ex805/TodoList.java
@@ -0,0 +1,27 @@
+package ex805;
+
+import java.util.ArrayList;
+
+public class TodoList {
+
+    ArrayList<String> list;
+
+    public TodoList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(String task) {
+        this.list.add(task);
+    }
+
+    public void print() {
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println((i + 1) + ": " + this.list.get(i));
+        }
+    }
+
+    public void remove(int number) {
+        this.list.remove(number - 1);
+    }
+}
+
