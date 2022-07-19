a89cbe0659d160ecbbc1be8b5949e1626f12232d
slawo <slawekpettke@gmail.com>
2022-07-19 19:16:43 +0200

    ---

---
+++ src/main/java/ex8_09/Program.java
@@ -0,0 +1,14 @@
+package ex8_09;
+
+import java.util.HashMap;
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Test your program here!
+    }
+
+    public static void printValues (HashMap<String, Book> hashmap) {}
+
+
+}
