b565ac288665aa17ce3419a9bdaefd868c545319
slawo <slawekpettke@gmail.com>
2022-07-21 13:29:16 +0200

    ---

---
+++ src/main/java/EX811/MainSimpleDate.java
@@ -0,0 +1,12 @@
+package EX811;
+
+public class MainSimpleDate {
+    public static void main(String[] args) {
+
+        SimpleDate d = new SimpleDate(1, 2, 2000);
+        System.out.println(d.equals("heh"));
+        System.out.println(d.equals(new SimpleDate(5, 2, 2012)));
+        System.out.println(d.equals(new SimpleDate(1, 2, 2000)));
+
+    }
+}
