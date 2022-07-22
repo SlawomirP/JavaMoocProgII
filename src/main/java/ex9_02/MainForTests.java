4272eba7852e5dc734c6411ebeef5d3aea18d5b7
slawo <slawekpettke@gmail.com>
2022-07-22 13:01:51 +0200

    ---

--- src/main/java/ex9_02/MainForTests.java
+++ src/main/java/ex9_02/MainForTests.java
@@ -2,28 +2,21 @@ package ex9_02;
 
 import java.util.ArrayList;
 
+//KLASA MAIN WYKORZYSTUJACA POPRZEDNIE KLASY
 public class MainForTests {
     public static void main(String[] args) {
 
-        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-        System.out.println(ada);
-        System.out.println(esko);
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-        int i = 0;
-        while (i < 25) {
-            ollie.study();
-            i = i + 1;
-        }
-        System.out.println(ollie);
-
-
+        printPersons(persons);
     }
 
     //METODA KTORA wyswietli wszystkie obiekty z listy w parametrze
-    public static void printPersons (ArrayList<Person> persons){
-
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person person : persons) {
+            System.out.println(person);
+        }
     }
 }
