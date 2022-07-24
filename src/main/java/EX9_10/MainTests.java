4dd8eb8505cfdb60bf983a03073cb6a23d29241d
slawo <slawekpettke@gmail.com>
2022-07-24 19:10:04 +0200

    ---

--- src/main/java/EX9_10/MainTests.java
+++ src/main/java/EX9_10/MainTests.java
@@ -4,13 +4,19 @@ public class MainTests {
     public static void main(String[] args) {
 
         Warehouse warehouse = new Warehouse();
-        warehouse.addProduct("milk", 3, 10);
-        warehouse.addProduct("coffee", 5, 7);
+        warehouse.addProduct("coffee", 5, 1);
 
-        System.out.println("prices:");
-        System.out.println("milk: " + warehouse.price("milk"));
-        System.out.println("coffee: " + warehouse.price("coffee"));
-        System.out.println("sugar: " + warehouse.price("sugar"));
+        System.out.println("stock:");
+        System.out.println("coffee:  " + warehouse.stock("coffee"));
+        System.out.println("sugar: " + warehouse.stock("sugar"));
+
+        System.out.println("taking coffee " + warehouse.take("coffee"));
+        System.out.println("taking coffee " + warehouse.take("coffee"));
+        System.out.println("taking sugar " + warehouse.take("sugar"));
+
+        System.out.println("stock:");
+        System.out.println("coffee:  " + warehouse.stock("coffee"));
+        System.out.println("sugar: " + warehouse.stock("sugar"));
 
     }
 }
--- src/main/java/EX9_10/Warehouse.java
+++ src/main/java/EX9_10/Warehouse.java
@@ -2,6 +2,7 @@ package EX9_10;
 
 import java.util.HashMap;
 import java.util.Map;
+
 //TA KLASA TWORZY MAGAZYN RODZIELAJAC PRODUKT NA 2 OBIEKTY
 //WRZUCANE W HASHMAPY
 public class Warehouse {
@@ -28,5 +29,34 @@ public class Warehouse {
             return this.productWithPrice.get(product);
         }
         return -99;
+    }
+
+    //METODA KTORA BEDZIE ZWRACALA AKTUALNY STAN PRODUKTU NA MAGAZYNIE
+    //JEZELI PRODUKTU NIE MA W MAGAZYNIE TO ZWROCI 0
+    public int stock(String product) {
+        if (this.productWithStockBalance.containsKey(product)) {
+            return this.productWithStockBalance.get(product);
+        }
+        return 0;
+    }
+
+    //METODA KTORA ZMNIEJSZY STAN PRODUKTU KTORY OTRZYMA W PARAMETRZE O 1
+    //STAN NIE MOZE BYC MNIEJSZY NIZ 0,A POTEM ZWROCI TRUE
+    //JEZELI NIE BEDZIE PRODUKTU TO ZWROCI FALSE
+    public boolean take(String product) {
+        //SPRAWDZAM CZY JEST PRODUKT
+        if (this.productWithStockBalance.containsKey(product)) {
+            //JEZELI JEST TO ZAPISUJE WARTOSC POD NIEGO PRZYPISANA DO
+            //POMOCNICZEJ ZMIENNEJ
+            int smallerStock = this.productWithStockBalance.get(product);
+            //JEZELI TA ZMIENNA JEST WIEKSZA OD 0 TO ODEJMUJE 1
+            if (smallerStock > 0) {
+                smallerStock -= 1;
+                //NASTEPNIE DODAJE KLUCZ Z NOWA WARTOSCIA ZASTEPUJACY STARY
+                this.productWithStockBalance.put(product, smallerStock);
+                return true;
+            }
+        }
+        return false;
     }
 }
