c97a20c3c4541c81311dcc257678ce3267888182
slawo <slawekpettke@gmail.com>
2022-07-25 22:19:27 +0200

    ---

--- src/main/java/MainTesting.java
+++ src/main/java/MainTesting.java
@@ -2,21 +2,38 @@ import ex810.IOU;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class MainTesting {
     public static void main(String[] args) {
-        TaskTracker tracker = new TaskTracker();
-        tracker.add("Ada", 3);
-        tracker.add("Ada", 4);
-        tracker.add("Ada", 3);
-        tracker.add("Ada", 3);
+        Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
 
-        tracker.add("Pekka", 4);
-        tracker.add("Pekka", 4);
+// reading inputs
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
 
-        tracker.add("Matti", 1);
-        tracker.add("Matti", 2);
+            inputs.add(row);
+        }
 
-        tracker.print();
+// counting the number of values divisible by three
+        long numbersDivisibleByThree = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(number -> number % 3 == 0)
+                .count();
+
+// working out the average
+        double average = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+
+// printing out the statistics
+        System.out.println("Divisible by three " + numbersDivisibleByThree);
+        System.out.println("Average number: " + average);
     }
 }
