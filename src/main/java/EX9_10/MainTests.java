2346c669d535810dfb9e01355af8b23d2a78c702
slawo <slawekpettke@gmail.com>
2022-07-25 10:26:35 +0200

    ---

--- src/main/java/EX9_10/MainTests.java
+++ src/main/java/EX9_10/MainTests.java
@@ -1,23 +1,19 @@
 package EX9_10;
 
+import java.util.Scanner;
+
 public class MainTests {
     public static void main(String[] args) {
 
-        ShoppingCart cart = new ShoppingCart();
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProduct("coffee", 5, 10);
+        warehouse.addProduct("milk", 3, 20);
+        warehouse.addProduct("cream", 2, 55);
+        warehouse.addProduct("bread", 7, 8);
 
-        cart.add("buttermilk", 2);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        Scanner scanner = new Scanner(System.in);
 
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
-
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        Store store = new Store(warehouse, scanner);
+        store.shop("John");
     }
 }
---
+++ src/main/java/EX9_10/Store.java
@@ -0,0 +1,62 @@
+package EX9_10;
+
+import java.util.Scanner;
+
+//We now have all the parts we need for our "online store", except the store itself.
+// Let's make that next. Our store has a warehouse that includes all our products.
+// For each 'visit' we have a shopping cart. Every time the customer chooses a product
+// its added to their cart if its available in the warehouse.
+// At the same time, the stock in the warehouse is reduced by one.
+public class Store {
+
+    //OBIEKTY KLAS KTORE BEDA UZYWANE W "SKLEPIE"
+    private Warehouse warehouse;
+    private Scanner scanner;
+
+    //KONSTRUKTOR GDZIE OKRESLIMY MAGAZYN I SKANERA
+
+    public Store(Warehouse warehouse, Scanner scanner) {
+        this.warehouse = warehouse;
+        this.scanner = scanner;
+    }
+
+    //METODA OBSŁUGUJĄCA KLIENTA, COS JAK START(),
+    //W PARAMETRZE PODAJEMY NAZWE KLIENTA
+    public void shop (String customer){
+        //TWORZYMY KOSZYK DLA NOWEGO KLIENTA
+        ShoppingCart cart = new ShoppingCart();
+        //KOMUNIKATY POWITALNE
+        System.out.println("Welcome to the store " + customer);
+        System.out.println("our selection:");
+
+        //TA PETLA PRZELATUJE PRZEZ TO CO ZWROCI METODA PRODUCTS
+        //Z KLASY WAREHOUSE CZYLI:
+        //ZWROCI NAM PRODUKTY KTORE ZNAJDUJA SIE W MAGAZYNIE
+        //TE PRODUKTY TO KLUCZE Z HASHMAPY GDZIE ZNAJDUJA SIE
+        //PRODUKTY Z CENAMI
+        for(String product : this.warehouse.products()){
+            System.out.println(product);
+        }
+
+        //PETLA DO UZUPELNIANIA KOSZYKA
+        while (true){
+
+            System.out.println("What to put in the cart (press enter to go to the register): ");
+
+            String product = scanner.nextLine();
+
+            //WARUNEK WYJSCIA Z PETLI
+            if(product.isEmpty()){
+                break;
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
