18bd2e35fa6bd5be1a9bf322b7ff95b264f0435a
slawo <slawekpettke@gmail.com>
2022-08-02 10:12:49 +0200

    ---

--- src/main/java/WyrazeniaRegularne/WyrazeniaRegularne.java
+++ src/main/java/WyrazeniaRegularne/WyrazeniaRegularne.java
@@ -1,5 +1,8 @@
 package WyrazeniaRegularne;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WyrazeniaRegularne {
     public static void main(String[] args) {
 
@@ -16,5 +19,25 @@ public class WyrazeniaRegularne {
 
         //czy liczba wystepuje okreslona ilosc razy
         System.out.println("abbccccdddd11112222".matches(".*\\d{8}.*"));
+
+        //czy liczba wystepuje od 5 do 12 razy
+        System.out.println("abbccccdddd11112222".matches(".*\\d{5,12}.*"));
+
+        //czy jest odstep
+        System.out.println("abbccccdddd11112222".matches(".*\\s.*"));
+
+        //czy jest wszystko inne niz odstep
+        System.out.println("abbccccdddd11112222".matches(".*\\S.*"));
+
+        //czy jest dowolny ze znakow
+        System.out.println("abbccccdddd11112222".matches(".*\\w.*"));
+
+        //podstawowa klasa definujaca wzorzec to Pattern
+        //tworzymy obiekt patternu zeby poslugiwac sie nim wielokrotnie
+        Pattern pattern = Pattern.compile(".*\\w.*");
+        //nastepnie na tym obiekcie uzywamy metody matcher gdzie podajemy ciag do analizy
+        Matcher matcher = pattern.matcher("abbccccdddd11112222");
+        //na obiekcie matcher uzywamy metody matches i to wyswietlamy
+        System.out.println(matcher.matches());
     }
 }
