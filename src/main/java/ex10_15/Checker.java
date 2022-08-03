5b0a67d09695ed30ef31d06e5965e6454f01f4f8
slawo <slawekpettke@gmail.com>
2022-08-03 09:25:08 +0200

    ---

--- src/main/java/ex10_15/Checker.java
+++ src/main/java/ex10_15/Checker.java
@@ -5,13 +5,13 @@ public class Checker {
 
     Checker checker = new Checker();
 
-        System.out.println(checker.isDayOfWeek("abc"));
+        System.out.println(checker.timeOfDay("33:33:33"));
 
     }
 
-    //METODA SPRAWDZI
+    //METODA SPRAWDZI czy wpisany text jest jednym z tych textow w nawiasie
     public  boolean isDayOfWeek(String string){
-        if (string.matches("mon | tue | wed | thu | fri | sat | sun")){
+        if (string.matches("mon|tue|wed|thu|fri|sat|sun")){
             System.out.println("The form is correct.");
             return true;
         } else {
@@ -19,6 +19,33 @@ public class Checker {
             return false;
         }
     }
+
+    //METODA KTORA SPRAWDZI CZY ZNAKI W STRINGU TO SAMOGLOSKI
+    public boolean allVowels (String string){
+        if (string.matches("[aeiou]*")){
+            System.out.println("The form is correct.");
+            return true;
+        } else {
+            System.out.println("The form is incorrect.");
+            return false;
+        }
+    }
+
+    //METODA SPRAWDZAJACA POPRAWNOSC WYSWIETLANEJ GODZINY 23:59:59
+    public boolean timeOfDay (String string){
+        //pierwsza cyfra przedzial od 0 do 1, druga cyfra to zakres 0 d0 9
+        // znak lub pierwsza cyfra to 2 a druga nalezy do przedzialu 0-3
+        // nastepnie dwukropek plus cyfra z przedzialu 0-5 i kolejna cyfra 0=9
+        // po dwukropku ten sam skład
+        if (string.matches("([0-1]\\d|2[0-3]):[0-5]\\d:[0-5]\\d")){
+            System.out.println("The form is correct.");
+            return true;
+        } else {
+            System.out.println("The form is incorrect.");
+            return false;
+        }
+    }
+
 
 }
 
