ced4fd985dbb6ee91c5df22b3a84525e175e63af
slawo <slawekpettke@gmail.com>
2022-08-03 20:32:16 +0200

    ---

--- src/main/java/ex10_17/Hand.java
+++ src/main/java/ex10_17/Hand.java
@@ -7,54 +7,57 @@ import java.util.List;
 //KLASA KTORA REPREZENTUJE KARTY W CZYJEJS RECE
 //W KOLEJNYM KROKU TUTAJ TEZ MAM ZAIMPLEMENTOWAC
 //INTERFEJS COMPARABLE NO I NADPISAC METODE COMPARETO
-public class Hand implements Comparable<Hand>{
+public class Hand implements Comparable<Hand> {
 
     List<Card> cards;
 
-    public Hand (){
+    public Hand() {
         this.cards = new ArrayList<>();
     }
 
     //dodanie karty do reki
-    public void add(Card card){
+    public void add(Card card) {
         this.cards.add(card);
     }
 
     //wyswietlenie wszystkich kart , zrobie na strimie
-    public void print(){
-        cards.stream()
-                .forEach(card -> System.out.println(card));
+    public void print() {
+        cards.stream().forEach(card -> System.out.println(card));
     }
 
     //METODA KTORA POSORTUJE KARTY na collections
     //to wykorzystuje metode compareTo dlatego ja trzeba bylo nadpisac
     //w klasie card
-    public void sort (){
+    public void sort() {
         Collections.sort(this.cards);
     }
-//METODA MA POROWNAC SUME WARTOSCI KART W RECE
+
+    //METODA MA POROWNAC SUME WARTOSCI KART W RECE
     @Override
     public int compareTo(Hand compared) {
-        if (this.getSumValuesInHand() == compared.getSumValuesInHand()){
-            return 0;}
+        if (this.getSumValuesInHand() == compared.getSumValuesInHand()) {
+            return 0;
+        }
         if (this.getSumValuesInHand() > compared.getSumValuesInHand()) {
             return 1;
         } else {
             return -1;
         }
     }
+
     //METODA ZLICZY SUME WARTOŚCI W LISCIE, ZROBIE TO NA STRIMIE
     //POTEM UZYJE TEGO W METODZIE COMPARETO
-    private int getSumValuesInHand(){
+    private int getSumValuesInHand() {
         //pomocniczy int, robie na nim stream, mapuje liste kart na liste wartosci kart
         //nastepnie reducerem zliczam wartosci
         int sum = this.cards.stream().map(card -> card.getValue())
-                .reduce(0,(previousSum, value)-> previousSum + value);
+                .reduce(0, (previousSum, value) -> previousSum + value);
         return sum;
     }
 
-    //METODA KTORA POSORTUJE KARTY W RECE
-    public void sortBySuit (){
+    //METODA KTORA POSORTUJE KARTY W RECE NAJPIERW POD KATEM KOLORU
+    //POTEM ZADECYDUJE WARTOSC
+    public void sortBySuit() {
         //POSORTUJE LISTE POD WARUNKAMI
         Collections.sort(this.cards, (c1, c2) -> {
             if (c1.getSuit().ordinal() == c2.getSuit().ordinal()) {
